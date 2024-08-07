6a1427c47a6de1cee53c2f5fb1b0e1c5be1049c3
Sandeep Kumar Prajapati <sandeep.prajapati@lngconsultancy.co.in>
2024-08-07 16:26:34 +0530

    adding new file

---
+++ ZarCare Automation/Test/TestScripts/hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarCare_Automation.Test.TestScripts
+{
+    internal class hello
+    {
+    }
+}
